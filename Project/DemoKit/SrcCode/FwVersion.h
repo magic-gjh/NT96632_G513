dae4097e221dd8a29a04474a4bdfc732b98689c9
magic <759838472@qq.com>
2015-11-10 18:08:17 +0800

    建仓

---
+++ Project/DemoKit/SrcCode/FwVersion.h
@@ -0,0 +1,29 @@
+#ifndef _FW_VERSION_H
+#define _FW_VERSION_H
+
+
+//-----------------------------------------------------------------------------
+// DSC Firmware Version String
+//-----------------------------------------------------------------------------
+//#define DSC_FW_VERSION_STR          "DVPLS100B06.110531"
+//#define DSC_FW_VERSION_STR          "SL513.20140423.V0.01"
+//#define DSC_FW_VERSION_STR          "SL513.20140522.V0.01"
+//#define DSC_FW_VERSION_STR          "SL513.20140526.V0.01"
+//#define DSC_FW_VERSION_STR          "SL513.20140527.V0.01"
+//#define DSC_FW_VERSION_STR          "SL513.20140616.V0.01"
+//#define DSC_FW_VERSION_STR          "SL513.20140620.V0.01"
+//#define DSC_FW_VERSION_STR          "SL513.20140701.V0.01"
+//#define DSC_FW_VERSION_STR          "SL513.20140702.V0.01"
+//#define DSC_FW_VERSION_STR          "SL513.20140704.V0.01"
+//#define DSC_FW_VERSION_STR          "SL513.20140713.V0.01"
+//#define DSC_FW_VERSION_STR          "SL513.20140716.V0.01"
+//#define DSC_FW_VERSION_STR          "SL513.20140731.V0.01"
+//#define DSC_FW_VERSION_STR          "SL513.20140826.V0.01"
+//#define DSC_FW_VERSION_STR          "SL513.20141231.V0.01"
+//#define DSC_FW_VERSION_STR          "SL513.20150909.V0.01"
+//#define DSC_FW_VERSION_STR          "SL513.20150914.V0.02"
+#define DSC_FW_VERSION_STR          "SL513.20151109.V0.01"//Open debug message
+
+
+#endif
+
